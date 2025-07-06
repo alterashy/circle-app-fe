10ccd9321bc04df0fbf08997e74423d5be72b5ec
Syifa Maulaya <maulayasyifamail@gmail.com>
2025-07-06 17:05:22 +0700

    feat(app): wrap application in ThemeProvider for theme management

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { ThemeProvider } from "./components/ui/theme";
 
 const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
