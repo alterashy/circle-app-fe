b609871b3616be4d33e31db81dbd3fd9f4845b52
Syifa Maulaya <maulayasyifamail@gmail.com>
2025-07-07 09:12:50 +0700

    feat(ui): add Toaster component for notifications in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "./components/ui/theme";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
@@ -11,6 +12,7 @@ function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <Toaster duration={2000} position="top-right" theme="system" />
       </QueryClientProvider>
     </ThemeProvider>
   );
