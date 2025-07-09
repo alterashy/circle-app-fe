70628e0122763dd10614682167908f5fe0b7b56d
Syifa Maulaya <maulayasyifamail@gmail.com>
2025-07-10 04:19:36 +0700

    feat: add Vercel configuration for rewrites and update router initialization in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { createBrowserHistory, createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "./components/ui/theme";
 import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, history: createBrowserHistory() });
 
 function App() {
   return (
